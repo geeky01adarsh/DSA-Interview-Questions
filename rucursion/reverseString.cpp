9069fe6de79a8f8c6e087f32a4309caa03e44724
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-11-01 20:10:38 +0530

    Create reverseString.cpp

---
+++ rucursion/reverseString.cpp
@@ -0,0 +1,14 @@
+
+// 344. Reverse String 
+// LeetCode...
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l=0;
+        int r=s.size()-1;
+        while(l<r){
+            swap(s[l++],s[r--]);
+        }
+    }
+};
